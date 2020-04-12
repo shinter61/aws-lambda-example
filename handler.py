e70d06340600b4575f3388ea014b97bbd072a061
shinter61 <shinter61@gmail.com>
2020-04-13 05:32:32 +0900

    Change function name

--- handler.py
+++ handler.py
@@ -1,7 +1,7 @@
 import json
 
 
-def hello(event, context):
+def main(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
@@ -14,11 +14,3 @@ def hello(event, context):
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
